3d9ab0c46eead9a8d43c2b962a616bfd2a271409
jeanluckawel <jeanluckawel45@yahoo.com>
2023-10-11 08:36:50 -0800

    base laravel

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,27 @@
+<nav class="fixed flex justify-between py-6 w-full lg:px-48 md:px-12 px-4 content-center bg-secondary z-10">
+    <div class="flex items-center">
+        <img src='dist/assets/Logo_black.svg' alt="Logo" class="h-4" />
+    </div>
+    <ul class="font-montserrat items-center hidden md:flex">
+        <li class="mx-3 ">
+            <a class="growing-underline" href="../howitworks.html">
+                How it works
+            </a>
+        </li>
+        <li class="growing-underline mx-3">
+            <a href="../features.html">Features</a>
+        </li>
+        <li class="growing-underline mx-3">
+            <a href="../pricing.html">Pricing</a>
+        </li>
+    </ul>
+    <div class="font-montserrat hidden md:block">
+        <button class="mr-6">Login</button>
+        <button class="py-2 px-4 text-white bg-black rounded-3xl">
+            Signup
+        </button>
+    </div>
+    <div id="showMenu" class="md:hidden">
+        <img src='dist/assets/logos/Menu.svg' alt="Menu icon" />
+    </div>
+</nav>
