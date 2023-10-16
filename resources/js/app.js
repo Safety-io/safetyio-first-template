1f603ecfad352d8cd9aaea9e7aa77d0407983132
jeanluckawel <jeanluckawel45@yahoo.com>
2023-10-16 10:07:55 -0800

    fix index -> Faq

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,6 @@ import.meta.glob([
 ]);*/
 
 window.addEventListener("load", function () {
-    console.log(document.querySelector("#showMenu"));
-    document
-        .querySelector("#showMenu")
-        .addEventListener("click", function (event) {
-            document.querySelector("#mobileNav").classList.remove("hidden");
-        });
-
-    document
-        .querySelector("#hideMenu")
-        .addEventListener("click", function (event) {
-            document.querySelector("#mobileNav").classList.add("hidden");
-        });
 
     document.querySelectorAll('[toggleElement]').forEach((toggle) => {
         toggle.addEventListener("click", function (event) {
