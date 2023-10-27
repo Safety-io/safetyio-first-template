a811c89a05ed8e0ddf860d608a91c1e096a18ec7
paul <paulkaf84@gmail.com>
2023-10-27 11:58:56 +0200

    fix script url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 {{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     <link rel="stylesheet" href="{{ asset("/assets/app.css") }}">
-    <link rel="stylesheet" href="{{ asset("/assets/app.js") }}">
+    <script src="{{ asset("/assets/app.js") }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title')</title>
 </head>
