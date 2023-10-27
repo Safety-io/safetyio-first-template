acd863352654de9b927c3a66cc9ade37e3a49997
jeanluckawel <jeanluckawel45@yahoo.com>
2023-10-27 10:41:52 -0800

    add 's'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset("/assets/app.css") }}">
     <script src="{{ asset("/assets/app.js") }}"> </script>
     <script src="{{ asset("/assets/app.js") }}" defer> </script>
