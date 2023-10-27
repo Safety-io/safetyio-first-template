a9e1931f1e41c0e336ba4b7eadfa38e5525b4c01
jeanluckawel <jeanluckawel45@yahoo.com>
2023-10-27 10:53:37 -0800

    question

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -219,4 +219,25 @@
     </section>
     @include('layouts.footer')
 @endsection
-{{--</body>--}}
+
+<script>
+    window.addEventListener("load", function () {
+
+        document.querySelectorAll('[toggleElement]').forEach((toggle) => {
+            toggle.addEventListener("click", function (event) {
+                console.log(toggle);
+                const answerElement = toggle.querySelector("[answer]");
+                const caretElement = toggle.querySelector("img");
+                console.log(answerElement);
+                if (answerElement.classList.contains("hidden")) {
+                    answerElement.classList.remove("hidden");
+                    caretElement.classList.add("rotate-90");
+                } else {
+                    answerElement.classList.add("hidden");
+                    caretElement.classList.remove("rotate-90");
+                }
+            });
+        });
+    });
+</script>
+
