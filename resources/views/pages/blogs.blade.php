4e1ed098cb711a5732a63b49c4f847c4116cee2d
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-02 19:16:42 +0200

    blogs

--- resources/views/pages/blogs.blade.php
+++ resources/views/pages/blogs.blade.php
@@ -13,7 +13,7 @@
 <div class="max-w-[1170px] mx-auto px-4 sm:px-8 xl:px-0">
 <div class="max-w-[770px] mx-auto w-full text-center mb-12.5">
 <h1 class="font-bold text-heading-6 sm:text-heading-4 lg:text-heading-3 text-dark mb-4">
-Archive
+Nos Blogs
 </h1>
 <p>See all posts we have ever written.</p>
 </div>
@@ -21,7 +21,7 @@ Archive
 
 <div class="group">
 <div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-01.png" alt="image" class="w-full" />
+<img src="{{  asset('images/blog-01.png')}}" alt="image" class="w-full" />
 </div>
 <h4>
 <a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
@@ -38,7 +38,7 @@ industry...
 <div class="flex items-center gap-2.5">
 <div class="flex items-center gap-3">
 <div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
+<img src=" {{( asset('images/user-01.png'))}}" alt="user" />
 </div>
 <p class="text-sm">Adrio Devid</p>
 </div>
@@ -52,7 +52,7 @@ industry...
 
 <div class="group">
 <div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-10.png" alt="image" class="w-full" />
+    <img src="{{  asset('images/blog-03.png')}}" alt="image" class="w-full" />
 </div>
 <h4>
 <a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
@@ -69,7 +69,7 @@ industry...
 <div class="flex items-center gap-2.5">
 <div class="flex items-center gap-3">
 <div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
+<img src="{{( asset('images/user-01.png'))}}" alt="user" />
 </div>
 <p class="text-sm">Adrio Devid</p>
 </div>
@@ -83,7 +83,7 @@ industry...
 
 <div class="group">
 <div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-05.png" alt="image" class="w-full" />
+    <img src="{{  asset('images/blog-02.png')}}" alt="image" class="w-full" />
 </div>
 <h4>
 <a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
@@ -100,7 +100,7 @@ industry...
 <div class="flex items-center gap-2.5">
 <div class="flex items-center gap-3">
 <div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
+    <img src="{{  asset('images/blog-01.png')}}" alt="image" class="w-full" />
 </div>
 <p class="text-sm">Adrio Devid</p>
 </div>
@@ -112,284 +112,9 @@ industry...
 </div>
 </div>
 
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-09.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-teal-dark bg-teal/[0.08] font-medium text-sm py-1 px-3 rounded-full">Health</span
-                >
-</div>
-</div>
 
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-02.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-blue bg-blue/[0.08] font-medium text-sm py-1 px-3 rounded-full">Technology</span
-                >
-</div>
-</div>
 
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-06.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-green-dark bg-green/[0.08] font-medium text-sm py-1 px-3 rounded-full">Travel</span
-                >
-</div>
-</div>
 
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-11.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-cyan-dark bg-cyan/[0.08] font-medium text-sm py-1 px-3 rounded-full">Culture</span
-                >
-</div>
-</div>
-
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-07.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-green-dark bg-green/[0.08] font-medium text-sm py-1 px-3 rounded-full">Travel</span
-                >
-</div>
-</div>
-
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-04.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-purple-dark bg-purple/[0.08] font-medium text-sm py-1 px-3 rounded-full">Lifestyle</span
-                >
-</div>
-</div>
-
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-08.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-green-dark bg-green/[0.08] font-medium text-sm py-1 px-3 rounded-full">Travel</span
-                >
-</div>
-</div>
-
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-03.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-blue bg-blue/[0.08] font-medium text-sm py-1 px-3 rounded-full">Technology</span
-                >
-</div>
-</div>
-
-<div class="group">
-<div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
-<img src="../clarity-tailwind.preview.uideck.com/images/blog-12.png" alt="image" class="w-full" />
-</div>
-<h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
-<span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
-Stylish Kitchen And Dining Room With Functional Ideas
-</span>
-</a>
-</h4>
-<p>
-Lorem Ipsum is simply dummy text of the print and typesetting
-industry...
-</p>
-<div class="flex flex-wrap gap-3 items-center justify-between mt-4.5">
-<div class="flex items-center gap-2.5">
-<div class="flex items-center gap-3">
-<div class="flex w-6 h-6 rounded-full overflow-hidden">
-<img src="../clarity-tailwind.preview.uideck.com/images/user-01.png" alt="user" />
-</div>
-<p class="text-sm">Adrio Devid</p>
-</div>
-<span class="flex w-[3px] h-[3px] rounded-full bg-dark-2"></span>
-<p class="text-sm">Sep 10, 2025</p>
-</div>
-<span class="inline-flex text-cyan-dark bg-cyan/[0.08] font-medium text-sm py-1 px-3 rounded-full">Culture</span
-                >
-</div>
-</div>
 </div>
 <div class="flex justify-center">
 <div class="inline-flex items-center justify-center rounded-md border-[0.8px] border-gray-3 shadow-1 mt-17.5">
