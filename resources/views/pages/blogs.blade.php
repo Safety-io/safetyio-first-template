d2dde1234e09fdc2d57369d63b99dad826e432a1
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-02 22:07:38 +0200

    blogs

--- resources/views/pages/blogs.blade.php
+++ resources/views/pages/blogs.blade.php
@@ -7,6 +7,7 @@
 
 
 
+
 <main>
 
 <section class="pt-34 pb-17.5">
@@ -17,14 +18,106 @@ Nos Blogs
 </h1>
 <p>See all posts we have ever written.</p>
 </div>
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-11 gap-x-7.5">
+
+    <div class="pt-14 border-t border-gray-3">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-11 gap-x-7.5">
+
+            <a href="author.html" class="group">
+                <div class="border border-gray-3 rounded-[20px] bg-gray p-5 group-hover:bg-white group-hover:drop-shadow-1 group-hover:-translate-y-2 transition-all">
+                    <div class="flex flex-wrap items-center gap-8">
+                        <div class="w-25 h-25 rounded-full overflow-hidden">
+                            <img src="{{ asset('/images/BlogTri/desktop computer-rafiki.png') }}" alt="user" />
+                        </div>
+                        <div>
+                            <h3 class="font-semibold text-custom-xl text-dark mb-1">
+                                Programmation
+                            </h3>
+                            <span class="flex items-center gap-2 text-custom-sm mt-2.5">
+                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="../www.w3.org/2000/svg.html">
+                                <path d="M5.775 14.4501H2.575C2.325 14.4501 2.125 14.2501 2.125 14.0001V2.00009C2.125 1.75009 2.325 1.55009 2.575 1.55009H8.225V4.75009C8.225 5.05009 8.475 5.32509 8.8 5.32509H11.975V7.00009C11.975 7.30009 12.225 7.57509 12.55 7.57509C12.875 7.57509 13.125 7.32509 13.125 7.00009V4.90009C13.125 4.62509 13 4.35009 12.8 4.15009L9.3 0.750091C9.1 0.550091 8.825 0.450091 8.55 0.450091H2.55C1.7 0.425091 1 1.15009 1 2.00009V14.0001C1 14.8501 1.7 15.5751 2.575 15.5751H5.8C6.1 15.5751 6.375 15.3251 6.375 15.0001C6.375 14.6751 6.1 14.4501 5.775 14.4501ZM9.325 2.35009L11.2 4.20009H9.325V2.35009Z" fill />
+                                <path d="M14.7996 9.40015C14.5996 9.20015 14.3996 9.00015 14.1996 8.80015C14.0246 8.62515 13.8496 8.42515 13.6496 8.25015C13.5496 8.12515 13.3996 8.02515 13.2246 8.00015C13.0246 7.97515 12.8246 8.02515 12.6746 8.15015L8.32461 12.4751C8.19961 12.6001 8.12461 12.7251 8.07461 12.8751L7.44961 14.7751L7.34961 15.0751L7.52461 15.3001C7.59961 15.4001 7.74961 15.5501 8.02461 15.5501H8.12461L10.0996 14.9001C10.2496 14.8501 10.3996 14.7751 10.4996 14.6501L14.7996 10.3751C14.9246 10.2501 14.9996 10.0751 14.9996 9.87515C14.9996 9.70015 14.9246 9.52515 14.7996 9.40015ZM13.1246 9.30015C13.2246 9.40015 13.3246 9.50015 13.3996 9.60015C13.4996 9.70015 13.5996 9.80015 13.6996 9.90015L13.4246 10.1751L12.8496 9.60015L13.1246 9.30015ZM9.72461 13.8501L8.84961 14.1251L9.12461 13.2501L12.0246 10.3501L12.5996 10.9251L9.72461 13.8501Z" fill />
+                                </svg>
+                                03 Postes publiés
+                                </span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="author.html" class="group">
+                <div class="border border-gray-3 rounded-[20px] bg-gray p-5 group-hover:bg-white group-hover:drop-shadow-1 group-hover:-translate-y-2 transition-all">
+                    <div class="flex flex-wrap items-center gap-8">
+                        <div class="w-25 h-25 rounded-full overflow-hidden">
+                            <img src="{{ asset('/images/BlogTri/Innovation-amico.png') }}" alt="user" />
+                        </div>
+                        <div>
+                            <h3 class="font-semibold text-custom-xl text-dark mb-1">
+                                Design
+                            </h3>
+                            <span class="flex items-center gap-2 text-custom-sm mt-2.5">
+                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="../www.w3.org/2000/svg.html">
+                                <path d="M5.775 14.4501H2.575C2.325 14.4501 2.125 14.2501 2.125 14.0001V2.00009C2.125 1.75009 2.325 1.55009 2.575 1.55009H8.225V4.75009C8.225 5.05009 8.475 5.32509 8.8 5.32509H11.975V7.00009C11.975 7.30009 12.225 7.57509 12.55 7.57509C12.875 7.57509 13.125 7.32509 13.125 7.00009V4.90009C13.125 4.62509 13 4.35009 12.8 4.15009L9.3 0.750091C9.1 0.550091 8.825 0.450091 8.55 0.450091H2.55C1.7 0.425091 1 1.15009 1 2.00009V14.0001C1 14.8501 1.7 15.5751 2.575 15.5751H5.8C6.1 15.5751 6.375 15.3251 6.375 15.0001C6.375 14.6751 6.1 14.4501 5.775 14.4501ZM9.325 2.35009L11.2 4.20009H9.325V2.35009Z" fill />
+                                <path d="M14.7996 9.40015C14.5996 9.20015 14.3996 9.00015 14.1996 8.80015C14.0246 8.62515 13.8496 8.42515 13.6496 8.25015C13.5496 8.12515 13.3996 8.02515 13.2246 8.00015C13.0246 7.97515 12.8246 8.02515 12.6746 8.15015L8.32461 12.4751C8.19961 12.6001 8.12461 12.7251 8.07461 12.8751L7.44961 14.7751L7.34961 15.0751L7.52461 15.3001C7.59961 15.4001 7.74961 15.5501 8.02461 15.5501H8.12461L10.0996 14.9001C10.2496 14.8501 10.3996 14.7751 10.4996 14.6501L14.7996 10.3751C14.9246 10.2501 14.9996 10.0751 14.9996 9.87515C14.9996 9.70015 14.9246 9.52515 14.7996 9.40015ZM13.1246 9.30015C13.2246 9.40015 13.3246 9.50015 13.3996 9.60015C13.4996 9.70015 13.5996 9.80015 13.6996 9.90015L13.4246 10.1751L12.8496 9.60015L13.1246 9.30015ZM9.72461 13.8501L8.84961 14.1251L9.12461 13.2501L12.0246 10.3501L12.5996 10.9251L9.72461 13.8501Z" fill />
+                                </svg>
+                                05 Postes publiés
+                                </span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="author.html" class="group">
+                <div class="border border-gray-3 rounded-[20px] bg-gray p-5 group-hover:bg-white group-hover:drop-shadow-1 group-hover:-translate-y-2 transition-all">
+                    <div class="flex flex-wrap items-center gap-8">
+                        <div class="w-25 h-25 rounded-full overflow-hidden">
+                            <img src="{{ asset('/images/BlogTri/Online connection-amico.png') }}" alt="user" />
+                        </div>
+                        <div>
+                            <h3 class="font-semibold text-custom-xl text-dark mb-1">
+                                Reseau
+                            </h3>
+                            <span class="flex items-center gap-2 text-custom-sm mt-2.5">
+                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="../www.w3.org/2000/svg.html">
+                                <path d="M5.775 14.4501H2.575C2.325 14.4501 2.125 14.2501 2.125 14.0001V2.00009C2.125 1.75009 2.325 1.55009 2.575 1.55009H8.225V4.75009C8.225 5.05009 8.475 5.32509 8.8 5.32509H11.975V7.00009C11.975 7.30009 12.225 7.57509 12.55 7.57509C12.875 7.57509 13.125 7.32509 13.125 7.00009V4.90009C13.125 4.62509 13 4.35009 12.8 4.15009L9.3 0.750091C9.1 0.550091 8.825 0.450091 8.55 0.450091H2.55C1.7 0.425091 1 1.15009 1 2.00009V14.0001C1 14.8501 1.7 15.5751 2.575 15.5751H5.8C6.1 15.5751 6.375 15.3251 6.375 15.0001C6.375 14.6751 6.1 14.4501 5.775 14.4501ZM9.325 2.35009L11.2 4.20009H9.325V2.35009Z" fill />
+                                <path d="M14.7996 9.40015C14.5996 9.20015 14.3996 9.00015 14.1996 8.80015C14.0246 8.62515 13.8496 8.42515 13.6496 8.25015C13.5496 8.12515 13.3996 8.02515 13.2246 8.00015C13.0246 7.97515 12.8246 8.02515 12.6746 8.15015L8.32461 12.4751C8.19961 12.6001 8.12461 12.7251 8.07461 12.8751L7.44961 14.7751L7.34961 15.0751L7.52461 15.3001C7.59961 15.4001 7.74961 15.5501 8.02461 15.5501H8.12461L10.0996 14.9001C10.2496 14.8501 10.3996 14.7751 10.4996 14.6501L14.7996 10.3751C14.9246 10.2501 14.9996 10.0751 14.9996 9.87515C14.9996 9.70015 14.9246 9.52515 14.7996 9.40015ZM13.1246 9.30015C13.2246 9.40015 13.3246 9.50015 13.3996 9.60015C13.4996 9.70015 13.5996 9.80015 13.6996 9.90015L13.4246 10.1751L12.8496 9.60015L13.1246 9.30015ZM9.72461 13.8501L8.84961 14.1251L9.12461 13.2501L12.0246 10.3501L12.5996 10.9251L9.72461 13.8501Z" fill />
+                                </svg>
+                                10 Postes publiés
+                                </span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="author.html" class="group">
+                <div class="border border-gray-3 rounded-[20px] bg-gray p-5 group-hover:bg-white group-hover:drop-shadow-1 group-hover:-translate-y-2 transition-all">
+                    <div class="flex flex-wrap items-center gap-8">
+                        <div class="w-25 h-25 rounded-full overflow-hidden">
+                            <img src="{{ asset('/images/BlogTri/Mother Earth Day-cuate.png') }}" alt="user" />
+                        </div>
+                        <div>
+                            <h3 class="font-semibold text-custom-xl text-dark mb-1">
+                                Autres
+                            </h3>
+                            <span class="flex items-center gap-2 text-custom-sm mt-2.5">
+                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="../www.w3.org/2000/svg.html">
+                                <path d="M5.775 14.4501H2.575C2.325 14.4501 2.125 14.2501 2.125 14.0001V2.00009C2.125 1.75009 2.325 1.55009 2.575 1.55009H8.225V4.75009C8.225 5.05009 8.475 5.32509 8.8 5.32509H11.975V7.00009C11.975 7.30009 12.225 7.57509 12.55 7.57509C12.875 7.57509 13.125 7.32509 13.125 7.00009V4.90009C13.125 4.62509 13 4.35009 12.8 4.15009L9.3 0.750091C9.1 0.550091 8.825 0.450091 8.55 0.450091H2.55C1.7 0.425091 1 1.15009 1 2.00009V14.0001C1 14.8501 1.7 15.5751 2.575 15.5751H5.8C6.1 15.5751 6.375 15.3251 6.375 15.0001C6.375 14.6751 6.1 14.4501 5.775 14.4501ZM9.325 2.35009L11.2 4.20009H9.325V2.35009Z" fill />
+                                <path d="M14.7996 9.40015C14.5996 9.20015 14.3996 9.00015 14.1996 8.80015C14.0246 8.62515 13.8496 8.42515 13.6496 8.25015C13.5496 8.12515 13.3996 8.02515 13.2246 8.00015C13.0246 7.97515 12.8246 8.02515 12.6746 8.15015L8.32461 12.4751C8.19961 12.6001 8.12461 12.7251 8.07461 12.8751L7.44961 14.7751L7.34961 15.0751L7.52461 15.3001C7.59961 15.4001 7.74961 15.5501 8.02461 15.5501H8.12461L10.0996 14.9001C10.2496 14.8501 10.3996 14.7751 10.4996 14.6501L14.7996 10.3751C14.9246 10.2501 14.9996 10.0751 14.9996 9.87515C14.9996 9.70015 14.9246 9.52515 14.7996 9.40015ZM13.1246 9.30015C13.2246 9.40015 13.3246 9.50015 13.3996 9.60015C13.4996 9.70015 13.5996 9.80015 13.6996 9.90015L13.4246 10.1751L12.8496 9.60015L13.1246 9.30015ZM9.72461 13.8501L8.84961 14.1251L9.12461 13.2501L12.0246 10.3501L12.5996 10.9251L9.72461 13.8501Z" fill />
+                                </svg>
+                                10 Postes publiés
+                                </span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+<div class="grid mt-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-11 gap-x-7.5">
 
 <div class="group">
 <div class="mb-6 overflow-hidden rounded-[10px] transition-all group-hover:scale-105">
 <img src="{{  asset('images/blog-01.png')}}" alt="image" class="w-full" />
 </div>
 <h4>
-<a href="../clarity-tailwind.preview.uideck.com/blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
+<a href="" class="block text-dark font-bold text-xl mb-3.5">
 <span class="bg-gradient-to-r from-primary/50 to-primary/40 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]">
 Stylish Kitchen And Dining Room With Functional Ideas
 </span>
