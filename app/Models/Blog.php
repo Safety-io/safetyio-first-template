77bbfff2907c628627fe4bccf6ae6adb8e3d1783
paul <paulkaf84@gmail.com>
2023-11-03 21:26:58 +0200

    fix blog

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 /**
  * @method static create(array $array)
  */
@@ -20,7 +22,14 @@ class Blog extends Model
         'picture',
         'grade',
         'read_time',
+        'category',
     ];
 
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(
+            Category::class,
 
+        );
+    }
 }
